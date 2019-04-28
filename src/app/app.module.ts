55540227d44acfe60f9ac53ca7c3f4122f5d1af6
zh <320066350@qq.com>
2019-04-28 23:28:56 +0800

    首次提交

---
+++ src/app/app.module.ts
@@ -0,0 +1,29 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { NgZorroAntdModule } from 'ng-zorro-antd';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    NgZorroAntdModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent implements OnInit {
+  private collapsed: boolean = true;
+  
+  private tabs: string[] = ['Tab 1', 'Tab 2'];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  closeTab(tab: string): void {
+    this.tabs.splice(this.tabs.indexOf(tab), 1);
+  }
+
+  newTab(): void {
+    this.tabs.push('New Tab');
+  }
+}
